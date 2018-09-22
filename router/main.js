8b6a1926dae71a2d9fc09d54a3d00b4630be2c63
joel-an <rejoelve@gmail.com>
2018-09-22 21:37:04 +0900

    feat: MongoDB를 사용한 Book API 구현

--- router/main.js
+++ router/main.js
@@ -1,4 +1,4 @@
-module.exports = (app, fs) => {
+module.exports = (app, fs, Book) => {
     app.get('/', (req, res) => {
         var sess = req.session;
 
@@ -149,7 +149,75 @@ module.exports = (app, fs) => {
                 }else{
                     res.redirect('/');
                 }
-            })
+            });
         }
     });
-}
+
+    app.get('/api/books', (req, res) => {
+        Book.find((err, books) => {
+            if(err) {
+                return res.status(500).send({error: 'DB failure'});
+            }
+            res.json(books);
+        });
+    });
+
+    app.get('/api/books/:book_id', (req, res) => {
+        Book.findOne({_id: req.params.book_id}, (err, book) => {
+            if(err) return res.status(500).json({error: err});
+            if(!book) return res.status(404).json({error: 'book not found'});
+            res.json(book);
+        })
+    });
+
+    app.get('/api/books/author/:author', (req, res) => {
+        Book.find({author: req.params.author}, {_id: 0, title: 1, published_date: 1}, (err, books) => {
+            if(err) return res.status(500).json({error: err});
+            if(books.length === 0) return res.status(404).json({error: 'book not found'});
+            res.json(books);
+        });
+    });
+
+    app.post('/api/books', (req, res) => {
+        var book = new Book();
+        book.title = req.body.title;
+        book.author = req.body.author;
+        book.published_date = new Date(req.body.published_date);
+        
+        book.save( err => {
+            if(err) {
+                console.error(err);
+                res.json({result: 0});
+                return;
+            }
+
+            res.json({result: 1});
+        });
+    });
+
+    app.put('/api/books/:book_id', (req, res) => {
+        Book.findById(req.params.book_id, (err, book) => {
+            if(err) return res.status(500).json({ error: 'DB failure' });
+            if(!book) return res.status(404).json({ error: 'book not found' });
+
+            if(req.body.title) book.title = req.body.title;
+            if(req.body.author) book.author = req.body.author;
+            if(req.body.published_date) book.published_date = req.body.published_date;
+
+            book.save(err => {
+                if(err) exports.status(500).json({ error: 'fauled to update!'});
+                res.json({ message: 'book updated'});
+            });
+        });;
+
+    });
+
+    app.delete('/api/books/:book_id', (req, res) => {
+        Book.deleteOne({ _id: req.params.book_id }, (err, output) => {
+            if(err) return res.status(500).json({ error: 'database failure'});
+          
+
+            res.status(204).end();
+        })
+    });
+};
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var session = require('express-session');
 var fs = require("fs");
 
-app.set('veiws', __dirname + '/views');
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => {
+    console.log("Connected to mongodb server");
+});
+
+mongoose.connect('mongodb://localhost/mongodb_tutorial');
+var Book = require('./models/book');
+
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
@@ -15,7 +25,7 @@ var server = app.listen(3000, () => {
 app.use(express.static('public'));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
     secret: '@#@$MYSIGN#$%#%',
     resave: false,
@@ -23,4 +33,4 @@ app.use(session({
 }));
 
 
-var router = require('./router/main')(app, fs);
+var router = require('./router/main')(app, fs, Book);
